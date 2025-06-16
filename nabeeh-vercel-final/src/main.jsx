5508cc48e999685a824d68075b17124d5f838d3c
msaharti <nodinho@live.com>
2025-06-16 10:55:23 +0300

    Add files via upload

---
+++ nabeeh-vercel-final/src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NabeehAssistant from './NabeehAssistant';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NabeehAssistant />
+  </React.StrictMode>
+);
